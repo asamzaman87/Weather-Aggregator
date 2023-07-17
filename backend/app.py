ac48dc4eb6f8363eaf87465c99bdf272c512bd4c
Asam Zaman <88610200+asamzaman87@users.noreply.github.com>
2023-07-17 00:35:07 -0400

    Update app.py
    Final changes

--- backend/app.py
+++ backend/app.py
@@ -1,25 +1,28 @@
 from flask import Flask, render_template, request
 import requests
+from pymongo import MongoClient
 import plotly.express as px
 import pandas as pd
-#from pymongo import MongoClient
+import plotly.io as pio
 
 app = Flask(__name__)
 
 # Create a MongoClient to the running MongoDB instance
-#client = MongoClient('localhost', 27017)
-#db = client['weather_news_database']
-#collection = db['news']
-"""
+client = MongoClient('localhost', 27017)
+db = client['weather_news_database']
+collection = db['news']
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 # make sure MongoDB DB is running. Here are the steps:
-    # 1- /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    # 2- brew tap mongodb/brew
-    # 3- brew install mongodb-community
-    # 4- brew services start mongodb-community
+# 1- /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# 2- brew tap mongodb/brew
+# 3- brew install mongodb-community
+# 4- brew services start mongodb-community
 
 @app.route('/connect_api', methods=['GET', 'POST'])
 def connect_api():
@@ -27,14 +30,17 @@ def connect_api():
         city = request.form["city"]
 
         # First, try to find news data in the database
-        news_data = collection.find_one({"city": city})
+        news_data = collection.find_one({"city": city})['data']
 
         # If the news data isn't in the database, fetch it from the API and store it
         if not news_data:
             news_data = get_weather_news(city)
             collection.insert_one({"city": city, "data": news_data})
 
-        return render_template('news.html', news=news_data)
+        fig = mateo(city)
+        fig_html = pio.to_html(fig, full_html=False)
+
+        return render_template('news.html', news=news_data, fig=fig_html)
 
 
 def get_weather_news(city):
@@ -51,15 +57,9 @@ def get_weather_news(city):
 
     response = requests.request("GET", url, params=querystring)
     return response.json()
-"""
-if __name__ == '__main__':
-    app.run()
 
-#Location; this is where the user inputs the city
-#city = input("Enter a Location: ")
-city = 'miami'
 
-#Geocode; this translates the city into coordinates and sends that to mateo
+# Geocode; this translates the city into coordinates and sends that to mateo
 def get_coords(city):
     appid = '721680ff54a03bc78d8ee5423fd07e8f'
     url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={appid}'
@@ -71,42 +71,42 @@ def get_coords(city):
         cords = []
         cords.append(str(latitude))
         cords.append(str(longitude))
-        return(cords)
+        return (cords)
     else:
         print('Error occurred', r.status_code)
 
-#Open Mateo; this gets all the weather information
-cords = get_coords(city)
-URL = f'https://api.open-meteo.com/v1/forecast?latitude={cords[0]}&longitude={cords[1]}&daily=weathercode,' \
-      f'temperature_2m_max,temperature_2m_min,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&' \
-      f'precipitation_unit=inch&timezone=auto&past_days=7'
-l = requests.get(URL)
-if l.status_code == 200:
-    data1 = l.json()
-    #print(data1['daily_units'])
-    #home(data1)
-    variables = list(data1["daily_units"])
-    labels = list(data1['daily']['time'])
-    weather_values = list(data1['daily']['weathercode'])
-    tempmax_values = list(data1['daily']['temperature_2m_max'])
-    tempmin_values = list(data1['daily']['temperature_2m_min'])
-    wind_values = list(data1['daily']['windspeed_10m_max'])
-else:
-    print('Error occurred', l.status_code)
 
-iterations = (len(variables)-1)
-xvars = labels*iterations
-yvars = weather_values + tempmax_values + tempmin_values + wind_values
-# print(weather_values)
-# print(tempmin_values)
-# print(tempmax_values)
-# print(wind_values)
+# Open Mateo; this gets all the weather information
+def mateo(city):
+    cords = get_coords(city)
+    URL = f'https://api.open-meteo.com/v1/forecast?latitude={cords[0]}&longitude={cords[1]}&daily=weathercode,' \
+          f'temperature_2m_max,temperature_2m_min,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&' \
+          f'precipitation_unit=inch&timezone=auto&past_days=7'
+    l = requests.get(URL)
+    if l.status_code == 200:
+        data1 = l.json()
 
-#Displays Weather information in line graph
-df = pd.DataFrame({'Day': xvars, 'Value': yvars,
-    'Condition': ['Weather Codes WMO Code']*len(weather_values) + ['Min Temp °F']*len(tempmin_values) +
-             ['Max Temp °F']*len(tempmax_values) + ['Wind Speed Mph']*len(wind_values)})
-fig = px.line(df, x = 'Day', y = 'Value', color = 'Condition')
-fig.show()
+        variables = list(data1["daily_units"])
+        labels = list(data1['daily']['time'])
+        weather_values = list(data1['daily']['weathercode'])
+        tempmax_values = list(data1['daily']['temperature_2m_max'])
+        tempmin_values = list(data1['daily']['temperature_2m_min'])
+        wind_values = list(data1['daily']['windspeed_10m_max'])
+    else:
+        print('Error occurred', l.status_code)
+
+    iterations = (len(variables) - 1)
+    xvars = labels * iterations
+    yvars = weather_values + tempmax_values + tempmin_values + wind_values
+
+    # Displays Weather information in line graph
+    df = pd.DataFrame({'Day': xvars, 'Value': yvars,
+                       'Condition': ['Weather Codes WMO Code'] * len(weather_values) + ['Min Temp °F'] * len(
+                           tempmin_values) +
+                                    ['Max Temp °F'] * len(tempmax_values) + ['Wind Speed Mph'] * len(wind_values)})
+    fig = px.line(df, x='Day', y='Value', color='Condition')
+    return fig
 
 
+if __name__ == '__main__':
+    app.run()
